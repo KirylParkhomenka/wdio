4a0d82d0689cda078a15cb28d07863d53092675d
Kiryl Parkhomenka <kirjan3@gmail.com>
2019-05-15 15:05:37 +0300

    Update config

--- wdio.conf.js
+++ wdio.conf.js
@@ -77,7 +77,6 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error
-    sync: true,
     logLevel: 'error',
     //
     // Warns when a deprecated command is used
