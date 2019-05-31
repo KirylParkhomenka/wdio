4436133f8d74751db53e4601d3223c0cbd3e271b
Kiryl Parkhomenka <kiryl.parkhomenka@coxautoinc.com>
2019-05-31 17:34:09 +0300

    Add tests

--- src/pages/MmrMainPage.js
+++ src/pages/MmrMainPage.js
@@ -5,13 +5,22 @@ class MmrMainPage extends Page {
     get vinInput() { return $('#vinText'); }
     get searchButton() { return $('.styles__vinEntry__3RUrd button'); }
     get similarVehicleComponent() { return $('.similarVehiclesDesktop'); }
-    get similarVehicleHeading() { return $('.similarVehiclesDesktop span'); }
+    get similarVehicleHeading() { return $('div[class*="desktopText"] span'); }
+    get viewAllLink() { return $('div[class*="desktopText"] a'); }
+
+    get oveLoginInput() { return $('#user_username'); }
+    get ovePasswordInput() { return $('#user_password'); }
+    get oveLoginButton() { return $('#login_button'); }
+    get oveResults() { return $('.ResultsCount__text'); }
+
     get adjustedText() { return $('div[class*="vehicleMmr"]'); }
     get vehicleModel() { return $('div[class*="vehicleModel"]'); }
-    get vehicleImage() { return $('div[class*="vehicleImage img"]'); }
+    get vehicleImage() { return $('div[class*="vehicleImage"] img'); }
+    get vehicleOdometer() { return $('.vehicleOdometer span'); }
 
     openMmrPage(URL) {
       super.open(URL);
+      browser.setWindowSize(1920, 1200);
     }
 
     performSearchByTestVin(TEST_VIN) {
@@ -24,8 +33,31 @@ class MmrMainPage extends Page {
       return this.similarVehicleComponent.isDisplayed();
     }
 
-    isSimilarVehicleHeadingDisplayed() {      
-      return this.similarVehicleHeading.isDisplayed();
+    getHeadingText() {      
+      return this.similarVehicleHeading.getText();
+    }
+
+    getViewAllLinkText() {      
+      return this.viewAllLink.getText();
+    }
+
+    verifyViewAllLinkLeadsToSrp(LOGIN, PASSWORD) {
+      this.viewAllLink.click();
+      browser.pause(2000);
+      let openedTabsHandles = browser.getWindowHandles();
+      browser.switchToWindow(openedTabsHandles[1]);
+
+      this.oveLoginInput.waitForDisplayed(5000);
+      this.oveLoginInput.setValue(LOGIN);
+      this.ovePasswordInput.setValue(PASSWORD);
+      this.oveLoginButton.click();
+      browser.pause(1000);
+
+      let url = browser.getUrl();
+
+      browser.switchToWindow(openedTabsHandles[0]);
+
+      return url;
     }
 
     isAdjustedTextDisplayed() {      
@@ -39,9 +71,18 @@ class MmrMainPage extends Page {
       return this.vehicleModel.getText().substring(0, 3);
     }
 
-    verifyImageIsDisplayedAndHasFixedSize() {
-      //image is displayed
-      //has size=w86h64
+    verifyImageHasFixedSize() {
+      let width = this.vehicleImage.getSize('width');
+      let height = this.vehicleImage.getSize('height');
+
+      return (width + 'x' + height);
+    }
+
+    verifyVehicleOdometerUnits() {
+      let odometerText = this.vehicleOdometer.getText();
+      let firstPart = odometerText.substring(0, odometerText.indexOf('|'));
+      let unit = firstPart.replace(/[0-9,. ]/g,'')
+      return unit;
     }
 }
 export default new MmrMainPage();
--- test/similarVehicleComponent.js
+++ test/similarVehicleComponent.js
@@ -1,59 +1,77 @@
+import MmrMainPage from '../src/pages/MmrMainPage';
 import { assert } from 'chai';
 import { expect } from 'chai';
-import MmrMainPage from '../src/pages/MmrMainPage';
 
 const URL = 'https://mmr.manheim.man-qa2.com/';
-const TEST_VIN = 'JHMZF1C6XBS016143';
+const TEST_VIN = 'JH4CL96855C010839';
+const COMPONENT_HEADING = 'Similar Vehicles for Sale';
+const OVE_LOGIN = 'demodealer';
+const OVE_PASSWORD = 'password';
+const VIEW_ALL_LINK = 'View All >';
+const SRP_URL_PART = 'search/results';
 
-describe('Mmr Main Page', () => {
+describe('Similar Vehicles component verification', () => {
   
-  it('Vidget is displayed', () => {
+  it('Component is displayed', () => {
+    MmrMainPage.openMmrPage(URL);
+    MmrMainPage.performSearchByTestVin(TEST_VIN);
+    
+    assert.isTrue(MmrMainPage.isSimilarVehicleComponentDisplayed());
+  });
 
-      MmrMainPage.openMmrPage(URL);
-      MmrMainPage.performSearchByTestVin(TEST_VIN);
-      
-      assert.isTrue(MmrMainPage.isSimilarVehicleComponentDisplayed());
-    });
+  it('Component Heading is "Similar Vehicles for Sale"', () => {
+    assert.equal(MmrMainPage.getHeadingText(), COMPONENT_HEADING);
+  });
 
-    it('Component Heading is displayed', () => {
+  it('Horizontal scroll button is displayed', () => {
+    //TBD
+  });
 
-      //Add title equals 'Similar Vehicles for Sale'
-      assert.isTrue(MmrMainPage.isSimilarVehicleHeadingDisplayed());
-    });
+  it('"View All >" link is displayed', () => {
+    assert.equal(MmrMainPage.getViewAllLinkText(), VIEW_ALL_LINK);
+  });
 
-    it('Horizontal scroll button is displayed', () => {
-      //TBD
-    });
+  it('"View All >" link navigates to an SRP', () => {
+    assert.include(MmrMainPage.verifyViewAllLinkLeadsToSrp(OVE_LOGIN, OVE_PASSWORD), SRP_URL_PART);
+  });
 
-    it('"View All>" link is displayed and navigates to an SRP', () => {
-      //TBD
-      //isDisplayed
-      //navigates to 
-    });
+  it('Adjusted MMR of vehicle is displayed', () => {
+    assert.isTrue(MmrMainPage.isAdjustedTextDisplayed());
+  });
 
-    it('Adjusted MMR of vehicle is displayed', () => {
-      assert.isTrue(MmrMainPage.isAdjustedTextDisplayed());
-    });
+  it('Year - first two digits are replaced by apostrophe (\')', () => {
+    assert.match(MmrMainPage.verifyApostrophReplaceTwoDigits(), /^["'][0-9]{2}$/);
+  });
 
-    it('Year - first two digits are replaced by apostrophe (\')', () => {
-      assert.match(MmrMainPage.verifyApostrophReplaceTwoDigits(), /^["'][0-9]{2}$/);
-    });
+  it('Model truncated', () => {
+    //TBD
+  });
 
-    it('Model truncated', () => {
-      //TBD
-    });
+  it('Trim truncated', () => {
+    //TBD
+  });
 
-    it('Trim truncated', () => {
-      //TBD
-    });
+  it('Vehicle image has fixed size 122x97 or 122x101', () => {
+    expect(MmrMainPage.verifyImageHasFixedSize()).to.be.oneOf(['122x97', '122x101']);
+  });
 
-    it('Vehicle image is displayed', () => {
-      //TBD
-    });
+  it('Odometer with units label is displayed (mi/km/hrs)', () => {
+    expect(MmrMainPage.verifyVehicleOdometerUnits()).to.be.oneOf(['mi', 'km', 'hrs']);
+  });
 
-    it('Odometer with units label is displayed (mi/km/hrs)', () => {
-      //TBD
-      //(mi/km/hrs)
-    });
+  it('CR or SD and grade are displayed (if present)', () => {
+    // TBD
+    // CR 0.0-5.0 //example (CR 4.5)
+    // if displayed
+    // to.be.oneOF('CR 0.0', '')
+  });
+
+  it('Entire vehicle card links to VDP', () => {
+    // TBD
+  });
+
+  it('Final viewable vehicle partly truncated', () => {
+    // TBD
+  });
 
 });
